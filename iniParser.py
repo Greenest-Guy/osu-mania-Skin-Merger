15c1d2eb2a932c5a81c6cd55363419ae1cd99647
Caleb Greene <greenestcaleb@gmail.com>
2025-07-23 00:34:31 -0400

    initial commit

---
+++ iniParser.py
@@ -0,0 +1,231 @@
+'''
+Made for parsing osu!mania skin.ini files
+Github: https://github.com/Greenest-Guy
+'''
+
+import os
+
+
+class iniParser:
+    # returns the file as a string
+    @staticmethod
+    def getContents(file_path): 
+        with open(file_path, 'r', encoding="utf-8") as file:
+            content = file.read()
+
+        return content
+
+
+    # returns a list containing the files lines
+    @staticmethod
+    def getLines(file_path): 
+        with open(file_path, 'r', encoding="utf-8") as file:
+            content = file.readlines()
+        
+        return content
+
+
+    # returns line number
+    @staticmethod
+    def getLine(file_path, line): 
+        with open(file_path, 'r', encoding="utf-8") as file:
+            content = file.readlines()
+
+        return content[line - 1]
+    
+
+    # find skin.ini from dir path
+    def findSkinini(dir_path):
+        try:
+            for file in os.listdir(dir_path):
+                if os.path.isfile(os.path.join(dir_path, file)) and file.lower() == "skin.ini":
+                    return os.path.join(dir_path, file)
+                
+        except Exception as e:
+            return None
+    
+
+    # gets the value associated with a line
+    @staticmethod
+    def getLineValue(line): 
+        return line[(line.index(':')+1):].strip()
+
+
+    # gets the key associated with a line
+    @staticmethod
+    def getKeyValue(line):
+        return line[:line.index(':')]
+    
+
+    # gets the first instance of a value associated with a key
+    @staticmethod
+    def getValue(file_path, key):
+        for line in iniParser.getLines(file_path):
+            if line.startswith(f"{key}:"):
+                return iniParser.getLineValue(line)
+
+
+    # returns a list of all catagories
+    @staticmethod
+    def getCatagories(file_path): 
+        catagories = []
+        with open(file_path, 'r', encoding="utf-8") as file:
+            content = file.readlines()
+        
+        for line in content:
+            line = line.strip()
+            if line.startswith('[') and line.endswith(']') and line not in catagories:
+                catagories.append(line)
+
+        return catagories
+    
+    # returns directory path
+    @staticmethod
+    def getDirPath(file_path):
+        return os.path.dirname(file_path)
+    
+
+    # returns a list of all image paths (relating to notes or keys)
+    @staticmethod
+    def getImages(file_path): 
+        images = []
+
+        for line in iniParser.getLines(file_path):
+            if (line.startswith("NoteImage") or line.startswith("KeyImage")) and iniParser.getLineValue(line) not in images:
+                images.append(iniParser.getLineValue(line))
+        
+        return images
+
+
+    # returns a list of all image paths
+    @staticmethod
+    def getImagesPath(file_path):
+        images = []
+        for image in iniParser.getImages(file_path):
+            images.append(os.path.join(iniParser.getDirPath(file_path), image.replace('/', os.sep)) + '.png')
+        
+        return images
+    
+
+    @staticmethod
+    def getSectionImages(file_path, chunk):
+        images = []
+
+        for line in chunk.splitlines():
+            if (line.startswith("NoteImage") or line.startswith("KeyImage")) and iniParser.getLineValue(line) not in images:
+                images.append((os.path.join(file_path, iniParser.getLineValue(line)) + ".png").replace('/', os.sep))
+        
+        return images
+
+
+    @staticmethod
+    def getNewSectionImages(chunk):
+        new_chunk = []
+
+        for line in chunk.splitlines():
+            if line.startswith("NoteImage") or line.startswith("KeyImage"):
+                new_chunk.append(f"{iniParser.getKeyValue(line)}: merge_files{os.sep}{os.path.basename(iniParser.getLineValue(line))}")
+            
+            else:
+                new_chunk.append(line)
+        
+        return "\n".join(new_chunk)
+
+    # edits a key value
+    @staticmethod
+    def editValue(file_path, value_name, new_value):
+        lines = iniParser.getLines(file_path)
+        
+        for index, line in enumerate(lines):
+            if line.startswith(value_name + ":"):
+                lines[index] = f"{iniParser.getKeyValue(line)}: {new_value}\n"
+        
+        with open(file_path, 'w', encoding="utf-8") as file:
+            file.writelines(lines)
+
+
+    # returns a list of all keycounts in a skin.ini file
+    @staticmethod
+    def getKeys(file_path):
+        keys = []
+
+        for line in iniParser.getLines(file_path):
+            if line.lower().startswith("keys: "):
+                key = int(iniParser.getLineValue(line))
+                if key not in keys:
+                    keys.append(key)
+        
+        keys.sort()
+        
+        return keys
+
+
+    # seperates each [Mania] block into a dictionary with the key being the keycount that [Mania] block is associated with and the value being the block itself
+    @staticmethod
+    def dictKeyChunks(file_path):
+        key_chunks = {}
+        lines = iniParser.getLines(file_path)
+
+        current_chunk = []
+        current_key = None
+
+        # add non [Mania] Section(s)
+        for line in lines:
+            line = line.strip()
+            if not line.lower().startswith("[mania]"):
+                current_chunk.append(line)
+
+            else:
+                break
+
+        key_chunks[-1] = "\n".join(current_chunk)
+        current_chunk = []
+
+        for line in lines:
+            line = line.strip()
+
+            # if [Mania] line, append this line to current_chunk
+            if line.lower().startswith("[mania]"):
+                # if both current_chunk isnt empty and current_key isnt None, add the entire chunk to the dictionary and reset both variables
+                if (current_chunk and current_key is not None) and current_key not in key_chunks:
+                    key_chunks[current_key] = "\n".join(current_chunk)
+                    current_chunk = []
+                    current_key = None
+                    
+                current_chunk.append(line)
+
+            # if line starts with "keys: " extract key value and set current_key
+            elif line.lower().startswith("keys:"):
+                current_key = int(iniParser.getLineValue(line))
+                current_chunk.append(line)
+            
+            # if a chunk is already started add line to chunk
+            elif current_chunk:
+                current_chunk.append(line)
+            
+        # add last pending chunk
+        if current_chunk and current_key is not None:
+            key_chunks[current_key] = "\n".join(current_chunk)
+
+
+        return key_chunks
+
+    
+    @staticmethod
+    def replaceKeySection(skin_file_path, section, num):
+        sections = iniParser.dictKeyChunks(skin_file_path)
+        
+        if num not in sections:
+            sections[num] = iniParser.getNewSectionImages(section[num])
+            sections = dict(sorted(sections.items()))
+        
+        elif num in sections:
+            sections[num] = iniParser.getNewSectionImages(section[num])
+        
+        new_file = ""
+
+        for i in sections:
+            new_file += sections[i] + "\n"
+
+        with open(skin_file_path, 'w', encoding="utf-8") as file:
+            file.write(new_file)
---
+++ main.py
@@ -0,0 +1,276 @@
+from CTkToolTip import CTkToolTip
+from iniParser import iniParser
+from version import __version__
+from tkinter import filedialog
+from datetime import datetime
+from customtkinter import *
+from PIL import Image
+import os, shutil
+
+
+class SkinMerger(CTk):
+    def __init__(self):
+        super().__init__()
+        set_appearance_mode("dark")
+        self.geometry("900x400")
+        self.title(f"osu!mania Skin Merger v{__version__} by Greenest-Guy")
+        self.resizable(False, False)
+        self.FM = SkinMergerLogic(self)
+
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+
+        icon_path = os.path.join(dir_path, "merge_logo.ico")
+        self.iconbitmap(icon_path)
+
+        # Background Image
+        background_image = CTkImage(light_image=Image.open(os.path.join(dir_path, "BG.png")), 
+                                    dark_image=Image.open(os.path.join(dir_path, "BG.png")), 
+                                    size=(900, 400))
+        
+        CTkLabel(self, image=background_image, text="").place(x=0, y=0)
+
+        # BUTTON Base Skin
+        base_skin_button = CTkButton(self, width=150, height=25, bg_color="#090909", text="Base Skin", command=self.FM.selectBaseSkin,
+                                    fg_color="#ffffff", hover_color="#b1b1b1", text_color="#000000", font=("", 16))
+        
+        base_skin_button.place(x=135-base_skin_button.cget("width")/2, y=65)
+
+        CTkToolTip(base_skin_button, message="The merge skin will be added to this skin")
+
+        # BUTTON Merge Skin
+        merge_skin_button = CTkButton(self, width=150, height=25, bg_color="#090909", text="Merging Skin", command=self.FM.selectMergeSkin,
+                                       text_color="#000000", fg_color="#ffffff", hover_color="#b1b1b1", font=("", 16))
+        
+        merge_skin_button.place(x=135-base_skin_button.cget("width")/2, y=146)
+
+        CTkToolTip(merge_skin_button, message="Keycount from this skin gets added to the base skin")
+
+        # OPTION MENU Key Select
+        self.key_select = CTkOptionMenu(self, values=["N/A"], width=65, height=25, text_color="#000000", bg_color="#090909"
+                                   , font=("", 16), fg_color="#ffffff", button_color="#bebebe", button_hover_color="#929292", command=self.FM.updateTextbox)
+        
+        self.key_select.place(x=135-self.key_select.cget("width")/2, y=228)
+
+        CTkToolTip(self.key_select, message="The keycount being added to the base skin from the merge skin")
+
+        # BUTTON Merge
+        merge_button = CTkButton(self, width=150, height=25, bg_color="#090909", text="Merge",
+                                       text_color="#000000", fg_color="#ffffff", hover_color="#b1b1b1", font=("", 16), command=self.FM.merge)
+        
+        merge_button.place(x=135-base_skin_button.cget("width")/2, y=310)
+
+        CTkToolTip(merge_button, message="Add the selected keycount from the merge skin to the base skin!")
+
+        # Textbox
+        self.textbox = CTkTextbox(self, width=598, height=323, font=("", 16))
+        self.textbox.place(x=267, y=39)
+        self.textbox.configure(state="disabled")
+    
+
+    # Error Window Pop-up
+    def showErrorWindow(self, message: str):
+        if hasattr(self, "error_box"):
+            self.error_box.destroy()
+        
+        self.error_box = CTkFrame(self, width=300, height=120, fg_color="#2a0000", corner_radius=12, bg_color="#1d1e1e")
+        self.error_box.place(relx=0.5, rely=0.5, anchor="center")
+
+        error_label = CTkLabel(self.error_box, text=message, text_color="#ffcccc", wraplength=280, font=("", 14))
+        error_label.pack(pady=(15, 5), padx=10)
+
+        close_button = CTkButton(self.error_box, text="Close", width=100, fg_color="#ff4c4c",
+                                hover_color="#cc0000", corner_radius=32, command=self.error_box.destroy)
+        close_button.pack(pady=(0, 10))
+        
+
+class SkinMergerLogic:
+    def __init__(self, app):
+        self.app = app
+
+        # Paths
+        self.base_skin_path = None
+        self.merge_skin_path = None
+
+        # Keymodes
+        self.base_skin_keymodes = None
+        self.merge_skin_keymodes = None
+
+        # Selections
+        self.selected_keymode = None
+        self.merge_filepaths = []
+    
+
+    def selectBaseSkin(self):
+        try:
+            self.base_skin_path = filedialog.askdirectory(title="Select Base Skin Folder")
+            self.base_skin_keymodes = iniParser.getKeys(iniParser.findSkinini(self.base_skin_path))
+    
+        except Exception as e:
+            self.app.showErrorWindow("Not a valid skin folder")
+            return None
+        
+        self.updateTextbox()
+        
+
+    def selectMergeSkin(self):
+        try:
+            self.merge_skin_path = filedialog.askdirectory(title="Select Merge Skin Folder")
+            self.merge_skin_keymodes = iniParser.getKeys(iniParser.findSkinini(self.merge_skin_path))
+
+            keymodes_string = []
+            for i in self.merge_skin_keymodes:
+                keymodes_string.append(f"{i}k")
+
+            self.app.key_select.configure(values=keymodes_string)
+            self.app.key_select.set(keymodes_string[0])
+    
+        except Exception as e:
+            self.merge_skin_path = None
+            self.merge_skin_keymodes = None
+            self.app.key_select.configure(values=[])
+            self.app.key_select.set("N/A")
+            self.app.showErrorWindow("Not a valid skin folder")
+            return None
+        
+        self.updateTextbox()
+
+
+    def updateTextbox(self, _=None):
+        self.enableTextbox()
+        self.app.textbox.delete("0.0", "end")
+
+        # Base Skin Output
+        if self.base_skin_path is not None and self.base_skin_path != "" and iniParser.findSkinini(self.base_skin_path) != None:
+            self.app.textbox.insert("end", f"Selected Base Skin: {self.getFileName(self.base_skin_path)}\n")
+            self.app.textbox.insert("end", "Keymodes: ")
+            
+            for i in self.base_skin_keymodes:
+                self.app.textbox.insert("end", f"{i}k")
+
+                if self.base_skin_keymodes.index(i) != len(self.base_skin_keymodes) - 1:
+                    self.app.textbox.insert("end", f", ")
+        
+        else:
+            self.app.textbox.insert("end", f"Selected Base Skin: N/A\n")
+            self.app.textbox.insert("end", "Keymodes: N/A")
+        
+        self.app.textbox.insert("end", "\n\n")
+
+        # Merge Skin Output
+        if self.merge_skin_path is not None and self.merge_skin_path != "":
+            self.app.textbox.insert("end", f"Selected Merge Skin: {self.getFileName(self.merge_skin_path)} \n")
+            self.app.textbox.insert("end", "Keymodes: ")
+            
+            for i in self.merge_skin_keymodes:
+                self.app.textbox.insert("end", f"{i}k")
+
+                if self.merge_skin_keymodes.index(i) != len(self.merge_skin_keymodes) - 1:
+                    self.app.textbox.insert("end", f", ")
+
+        else:
+            self.app.textbox.insert("end", f"Selected Merge Skin: N/A\n")
+            self.app.textbox.insert("end", "Keymodes: N/A")
+        
+        self.app.textbox.insert("end", "\n\n")
+
+        # Merge Settings
+        self.app.textbox.insert("end", f"Selected Keymode to be Merged: {self.app.key_select.get()}")
+        
+        self.disableTextbox()
+    
+    
+    def merge(self):
+        # error
+        if iniParser.findSkinini(self.base_skin_path) is None and iniParser.findSkinini(self.merge_skin_path) is None:
+            self.app.showErrorWindow("Invalid base and merge skin")
+            return None
+        
+        elif iniParser.findSkinini(self.base_skin_path) is None:
+            self.app.showErrorWindow("Invalid base skin")
+            return None
+        
+        elif iniParser.findSkinini(self.merge_skin_path) is None:
+            self.app.showErrorWindow("Invalid merge skin")
+            return None
+        
+        if self.base_skin_path == self.merge_skin_path:
+            self.app.showErrorWindow("Base skin and merge skin cant be the same")
+            return None
+
+        keycount = int(self.app.key_select.get().strip('k'))
+        mania_chunks = iniParser.dictKeyChunks(iniParser.findSkinini(self.merge_skin_path))
+        name = f"Merged-Skin-{self.getTime()}"
+
+        # creates skin folder in downloads with files from base skin
+        downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+
+        new_skin_folder = os.path.join(downloads_folder, name)
+
+        try:
+            shutil.copytree(self.base_skin_path, new_skin_folder)
+        
+        except Exception as e:
+            self.app.showErrorWindow("Failed to copy base skin directory to downloads")
+            return None
+
+        # creates merge files folder
+        merge_files_folder = os.path.join(new_skin_folder, "merge_files")
+
+        try:
+            os.mkdir(merge_files_folder)
+        
+        except Exception as e:
+            self.app.showErrorWindow("Failed to make merge files folder")
+            return None
+
+        # gets needed_files and puts them in merge files folder
+        needed_files = iniParser.getSectionImages(self.merge_skin_path, mania_chunks[keycount])
+
+        missing_files = 0
+        for i in needed_files:
+            
+            try:
+                shutil.copy(i, merge_files_folder)
+            
+            except Exception as e:
+                missing_files += 1
+                self.app.showErrorWindow(f"Couldnt copy {missing_files} file(s)")
+        
+        # edits skin.ini file
+        skin_file_path = iniParser.findSkinini(new_skin_folder)
+        iniParser.replaceKeySection(skin_file_path, mania_chunks, keycount)
+
+        # Update Author
+        iniParser.editValue(skin_file_path, "Author", f"{iniParser.getValue(iniParser.findSkinini(self.base_skin_path), "Author")} + {iniParser.getValue(iniParser.findSkinini(self.merge_skin_path), "Author")}")
+        
+    
+    @staticmethod
+    def getFileName(file_path):
+        return os.path.basename(file_path)
+    
+    
+    @staticmethod
+    def getTime():
+        return datetime.now().strftime("%H-%M-%S")
+
+
+    def disableTextbox(self):
+        self.app.textbox.configure(state="disabled")
+    
+    
+    def enableTextbox(self):
+        self.app.textbox.configure(state="normal")
+
+
+if __name__ == "__main__":
+    app = SkinMerger()
+    app.mainloop()
+
+
+'''
+COLORS
+#ffffff - White
+#000000 - Black
+#1d1e1e - Textbox Color
+#090909 - Box Color
+'''
